9318f51be3fb020d0a8c9713eeb86816da653f38
rsdevalentim@gmail.com <rsdriver1000@gmail.com>
2021-07-25 20:22:00 -0300

    Aulas
    commitei minhas primeiras aulas de php

---
+++ aula004/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilor.css"/>
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <?php
+    $n = 4;
+    $no = "Rodrigo";
+    echo $no
+    ?>
+</div>
+</body>
+</html>
