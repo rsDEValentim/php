69afbee7aeea248c760fd212b081169e2497b92e
rsdevalentim@gmail.com <rsdriver1000@gmail.com>
2021-08-18 18:23:01 -0300

    estudando
    avançando na aula de php

--- aula005/funcoesaritmeticas.php
+++ aula005/funcoesaritmeticas.php
@@ -20,6 +20,13 @@
         $v2 = $_GET["y"];
         echo"<h2>Valores recebidos: $v1 e $v2</h2>";
         echo "O valor absoluto de $v2 e ". abs($v2);
+        echo "</br>O valor de $v1 <sup>$v2</sup> é ". pow($v1, $v2);
+        echo "</br> a raiz de $v1 é ". sqrt($v1);
+        echo "</br> O valor de $v2 arredondado é ". round($v2);
+        echo "</br> O valor de $v2 arredondado para cima é ". ceil($v2);
+        echo "</br> O valor de $v2 arredondado para baixo é ". floor($v2);
+        echo "</br> A parte inteira de $v2 é ". intval($v2);
+        echo "</br> O valor de $v1 em moeda é R$ " . number_format($v1, 2, ",", "."); 
     
     ?>
 
